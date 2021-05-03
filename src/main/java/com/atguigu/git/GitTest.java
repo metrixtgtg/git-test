7ce52f2ef290ed91a51d74df89c3e83ab86b43ce
metrix <metrix_nj@163.com>
2021-05-04 04:09:48 +0800

    hot-fix commit-2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("Hello GitHub!");
         System.out.println("Hello GitHub3!");
         System.out.println("Hello GitHub4!");
+        System.out.println("Hot-fix test");
+
 
 
     }
