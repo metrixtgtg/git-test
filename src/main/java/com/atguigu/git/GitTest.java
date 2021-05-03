4f2cf5bfd686b471ece093fe97807c2533b75fc8
metrix <metrix_nj@163.com>
2021-05-04 05:04:19 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("Hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
 
 
 
