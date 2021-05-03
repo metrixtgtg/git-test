8e805b43ca7c8e193450cd9440cedfec9910782d
metrix <metrix_nj@163.com>
2021-05-04 04:10:55 +0800

    master-commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("Hello GitHub!");
         System.out.println("Hello GitHub3!");
         System.out.println("Hello GitHub4!");
+        System.out.println("master test!");
 
 
     }
