adae57f7df3e8cba3759d5a200869bbbc9c6ede9
metrix <metrix_nj@163.com>
2021-05-04 08:03:35 +0800

    github commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("gitee test");
         System.out.println("gitee test2");
+        System.out.println("gihub test1");
 
 
 
